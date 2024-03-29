f5c51f046edf6b2a9994312f6141af64e5e73c68
k <c0p31034@bunkyo.ac.jp>
2024-03-29 21:57:08 +0900

    tailwindで少しデザイン作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,31 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col  p-6 text-sm sm:text-base">
-      <label className="font-bold mb-1.5 text-xs text-gray-400" htmlFor="">
-        メールアドレス
-      </label>
-      <input className="border-solid border border-gray-200 px-2 py-2 w-full rounded-md" placeholder="" type="email" name="email" id="" />
-      <button className="mt-6 duration-200  w-full rounded-full bg-tahiti-500 hover:bg-tahiti-600 text-white font-bold py-3 px-6 rounded">ログイン</button>
-    </main>
+    <>
+      <header className="h-20 w-full flex flex-col items-center justify-center bg-sky-300">
+        <h1 className="text-xl font-bold text-white">今日のニュース</h1>
+        <p className="text-xs text-white mt-1">2023年4月28日 金曜日</p>
+      </header>
+
+      <main className="flex gap-2 min-h-screen flex-col py-4 px-4 xl:px-96 text-sm sm:text-base bg-neutral-100">
+        <div className="bg-white flex rounded-lg overflow-hidden hover:bg-gray-50 cursor-pointer">
+          <Image src="/sample.png" width={132} height={132} alt="Picture of the author" />
+          <div className="flex flex-col justify-center px-3">
+            <p className="text-md sm:text-base font-bold text-neutral-800 line-clamp-2 leading-relaxed">
+              衣替えはGWに終わらせよう。どうせなら毛玉も一気に解消衣替えはGWに終わらせよう。どうせなら毛玉も一気に解消
+            </p>
+            <p className="text-xs sm:text-sm mt-1 text-gray-400">GIZMODO JAPAN</p>
+          </div>
+        </div>
+
+        <div className="bg-white flex rounded-lg overflow-hidden hover:bg-gray-50 cursor-pointer">
+          <Image src="/sample.png" width={132} height={132} alt="Picture of the author" />
+          <div className="flex flex-col justify-center px-3">
+            <p className="text-md sm:text-base font-bold text-neutral-900">衣替えはGWに終わらせよう。どうせなら毛玉も一気に解消</p>
+            <p className="text-xs sm:text-sm mt-1 text-gray-400">GIZMODO JAPAN</p>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,35 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
-
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "radial-gradient": "radial-gradient(2361.48% 189.21% at -3.97% -4.43%, #87A1D3 0%, #87A1D3 0.01%, #AADFF3 47.43%, #FFB8E3 100%)",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        'white': '#ffffff',
-        'purple': '#3f3cbb',
-        'midnight': '#121063',
-        'metal': '#565584',
-        'tahiti': {
-          100: '#cffafe',
-          200: '#a5f3fc',
-          300: '#67e8f9',
-          400: '#22d3ee',
-          500: '#06b6d4',
-          600: '#0891b2',
-          700: '#0e7490',
-          800: '#155e75',
-          900: '#164e63',
+        transparent: "transparent",
+        current: "currentColor",
+        gradation: "radial-gradient(2361.48% 189.21% at -3.97% -4.43%, #87A1D3 0%, #87A1D3 0.01%, #AADFF3 47.43%, #FFB8E3 100%)",
+        white: "#ffffff",
+        purple: "#3f3cbb",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: {
+          100: "#cffafe",
+          200: "#a5f3fc",
+          300: "#67e8f9",
+          400: "#22d3ee",
+          500: "#06b6d4",
+          600: "#0891b2",
+          700: "#0e7490",
+          800: "#155e75",
+          900: "#164e63",
         },
-        'silver': '#ecebff',
-        'bubble-gum': '#ff77e9',
-        'bermuda': '#78dcca',
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
       },
     },
   },
